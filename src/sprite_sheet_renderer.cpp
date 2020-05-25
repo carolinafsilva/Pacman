6bee7a06cb8eac9a6ad5ca24fdf1055a24c8c9ff
Carolina Silva <carolsilva.acfs@gmail.com>
2020-05-25 23:51:15 +0100

    Changed lib extension to .hpp

--- src/sprite_sheet_renderer.cpp
+++ src/sprite_sheet_renderer.cpp
@@ -6,7 +6,7 @@
 ** Creative Commons, either version 4 of the License, or (at your
 ** option) any later version.
 ******************************************************************/
-#include "sprite_sheet_renderer.h"
+#include "sprite_sheet_renderer.hpp"
 
 #include <iostream>
 
