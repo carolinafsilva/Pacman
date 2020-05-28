c4b7a29fb0fb507438c358e8f62a9170b479a35c
Carolina Silva <carolsilva.acfs@gmail.com>
2020-05-28 11:05:18 +0100

    Update maze matrix

--- src/maze.cpp
+++ src/maze.cpp
@@ -1,24 +1,19 @@
 #include "maze.hpp"
 
-glm::vec3 Maze::Up(glm::vec3 position, float delta) {
-  // FIXME: convert to matrix index
-  // up.x = position.x;
-  // up.y = position.y - 1;
-  // return up;
-}
-glm::vec3 Maze::Left(glm::vec3 position, float delta) {
+glm::vec2 Maze::Up(glm::vec3 position) {}
+glm::vec2 Maze::Left(glm::vec3 position) {
   // FIXME: convert to matrix index
   // left.x = position.x - 1;
   // left.y = position.y;
   // return left;
 }
-glm::vec3 Maze::Down(glm::vec3 position, float delta) {
+glm::vec2 Maze::Down(glm::vec3 position) {
   // FIXME: convert to matrix index
   // down.x = position.x;
   // down.y = position.y + 1;
   // return down;
 }
-glm::vec3 Maze::Right(glm::vec3 position, float delta) {
+glm::vec2 Maze::Right(glm::vec3 position) {
   // FIXME: convert to matrix index
   // right.x = position.x + 1;
   // right.y = position.y;
@@ -36,12 +31,10 @@ float Maze::euclidianDist(glm::vec3 position1, glm::vec3 position2) {
   return sqrtf(powf(center1.x - center2.x, 2) + powf(center1.y - center2.y, 2));
 }
 
-bool Maze::valid(glm::vec3 position) {
-  // if (position->x >= 0 && position->x < BOARD_X && position->y >= 0 &&
-  //    position->y < BOARD_Y) {
-  // FIXME: convert to matrix index
-  // return this->matrix[position->x][position->y] != -1;
-  //}
+bool Maze::valid(glm::vec2 block) {
+  if (block.x >= 0 && block.x < BLOCK_L && block->y >= 0 && block.y < BLOCK_C) {
+    return this->matrix[block.x][block.y] >= 0;
+  }
   return false;
 }
 
