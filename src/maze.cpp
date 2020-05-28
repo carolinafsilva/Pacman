537f5352b5d7328774fed71c7905ad8b1caaa34c
Carolina Silva <carolsilva.acfs@gmail.com>
2020-05-28 09:42:18 +0100

    Add utility functions

--- src/maze.cpp
+++ src/maze.cpp
@@ -25,6 +25,17 @@ glm::vec3 Maze::Right(glm::vec3 position, float delta) {
   // return right;
 }
 
+glm::vec2 Maze::getCenter(glm::vec3 position) {
+  return glm::vec2(position.x + position.z / 2, position.y + position.z / 2);
+}
+
+float Maze::euclidianDist(glm::vec3 position1, glm::vec3 position2) {
+  glm::vec2 center1 = Maze::getCenter(position1);
+  glm::vec2 center2 = Maze::getCenter(position2);
+
+  return sqrtf(powf(center1.x - center2.x, 2) + powf(center1.y - center2.y, 2));
+}
+
 bool Maze::valid(glm::vec3 position) {
   // if (position->x >= 0 && position->x < BOARD_X && position->y >= 0 &&
   //    position->y < BOARD_Y) {
